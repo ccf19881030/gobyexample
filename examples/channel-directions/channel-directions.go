0242e81536bcaf837cd50e9436d9cee2789ad874
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2020-08-15 22:08:59 +0800

    Update channel-directions.go
    此处的注释好像是弄反了吧。

--- examples/channel-directions/channel-directions.go
+++ examples/channel-directions/channel-directions.go
@@ -5,13 +5,13 @@ package main
 
 import "fmt"
 
-// `ping` 函数定义了一个只能发送数据的（只写）通道。
-// 尝试从这个通道接收数据会是一个编译时错误。
+// `ping` 函数定义了一个只能接收数据的（只写）通道。
+// 尝试从这个通道发送数据会是一个编译时错误。
 func ping(pings chan<- string, msg string) {
 	pings <- msg
 }
 
-// `pong` 函数接收两个通道，`pings` 仅用于接收数据（只读），`pongs` 仅用于发送数据（只写）。
+// `pong` 函数接收两个通道，`pings` 仅用于发送数据（只读），`pongs` 仅用于接收数据（只写）。
 func pong(pings <-chan string, pongs chan<- string) {
 	msg := <-pings
 	pongs <- msg
